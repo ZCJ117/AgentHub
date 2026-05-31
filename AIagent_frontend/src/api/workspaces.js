c56c68c3c9522976d4af1fec9189b43edd559fc6
86178 <1534668781@qq.com>
2026-05-31 15:37:38 +0800

    feat: add updateWorkspace API function

--- AIagent_frontend/src/api/workspaces.js
+++ AIagent_frontend/src/api/workspaces.js
@@ -3,3 +3,7 @@ import apiClient from './client'
 export function fetchWorkspaces() {
   return apiClient.get('/api/v1/workspaces')
 }
+
+export function updateWorkspace(id, data) {
+  return apiClient.put(`/api/v1/workspaces/${id}`, data)
+}
