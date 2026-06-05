efd136492a0af40c0ded0c142e3212a064649357
86178 <1534668781@qq.com>
2026-06-05 20:24:53 +0800

    fix: add NMessageProvider to App.vue for Composer useMessage

--- AIagent_frontend/src/App.vue
+++ AIagent_frontend/src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
-import { NConfigProvider } from 'naive-ui'
+import { NConfigProvider, NMessageProvider } from 'naive-ui'
 import { themeOverrides } from '@/assets/styles/naive-theme'
 import PageTransition from '@/components/common/PageTransition.vue'
 </script>
 
 <template>
   <NConfigProvider :theme-overrides="themeOverrides">
-    <PageTransition />
+    <NMessageProvider>
+      <PageTransition />
+    </NMessageProvider>
   </NConfigProvider>
 </template>
